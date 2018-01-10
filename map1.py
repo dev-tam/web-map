7ef3791d12153bfe8d3772e0f7f454f7489f3a9f
dev-tam <tamjid.mdh@gmail.com>
2018-01-10 23:55:16 +0600

    layer control

--- map1.py
+++ map1.py
@@ -6,6 +6,7 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+
 def color_producer(elevation):
     if elevation < 1000:
         return 'green'
@@ -14,13 +15,26 @@ def color_producer(elevation):
     else:
         return 'red'
 
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
-for lt,ln,el in zip(lat,lon,elev):
-    fg.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m", icon=folium.Icon(color=color_producer(el))))
+for lt, ln, el in zip(lat, lon, elev):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el) + " m", fill_color=color_producer(el),
+                                      color='grey', fill_opacity=0.7))
 
-map.add_child(fg)
+
+fgp = folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open('world.json', encoding='utf-8-sig').read(),
+                             style_function=lambda x: {'fillColor': 'green' if x['properties'][
+                                                                                  'POP2005'] < 10000000 else 'orange' if 10000000 <=
+                                                                                                                        x[
+                                                                                                                            'properties'][
+                                                                                                                            'POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
